081a932c47fc04d9ce976b40e08137190086fad8
mahdi darafshi <m.darafshi@gmail.com>
2020-02-12 14:40:13 +0330

    added a post method: creatinh one

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -16,8 +16,20 @@ router.get('/:id', (req , res)=>{
     res.send(req.params.id)
 })
 // creating one
-router.get('/', (req , res)=>{
-    
+router.get('/',async (req , res)=>{
+    const subscriber=new Subscriber({
+        name: req.body,
+        subscribedToChannel: req.body.subscribedToChannel
+    })
+    try{
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+        // 201: object succesfully created.
+    }
+    catch(err){
+        res.status(400).json({message: err.message})
+        // 400: something wrong in user input or server!
+    }
 })
 
 // updating one
